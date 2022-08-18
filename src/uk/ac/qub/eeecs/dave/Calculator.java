483b8a989380dcd7ef63f8fc45382967daa7d148
40223826 <109058388+40223826@users.noreply.github.com>
2022-08-18 19:11:31 +0100

    Update Calculator.java
    Plus 1 to answer

--- src/uk/ac/qub/eeecs/dave/Calculator.java
+++ src/uk/ac/qub/eeecs/dave/Calculator.java
@@ -2,19 +2,19 @@ package uk.ac.qub.eeecs.dave;
 
 public class Calculator {
 	public int add(int a, int b) {
-		return a+b;
+		return a+b+1;
 	}
 	
 	public int sub(int a, int b) {
-		return a-b;
+		return a-b+1;
 	}
 	
 	public int multiply(int a, int b) {
-		return a*b;
+		return a*b+1;
 	}
 	
 	public int divide (int a, int b) {
-		return a/b;
+		return a/b+1;
 	}
 
 }
